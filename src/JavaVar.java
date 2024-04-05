044f5c9012a28ff1e3a4deba6cc57e69fbff8553
Amberly <160063986+amberlyhuang47562@users.noreply.github.com>
2024-04-05 13:22:55 -0400

    creation of the datatypes and assigning them values

---
+++ src/JavaVar.java
@@ -0,0 +1,16 @@
+
+public class JavaVar 
+{
+// datatypes
+    public static void main(String[] args) throws Exception 
+    {
+        // datatype varName [=value]; 
+        int games = 3; 
+        double allowance = 40.50;
+        long battlepass; 
+
+        battlepass = 15;
+
+        System.out.println(games);
+    }
+}
