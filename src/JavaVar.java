622ad1281f4a9814c638c4f2b48ab136b96e42a6
Amberly <160063986+amberlyhuang47562@users.noreply.github.com>
2024-04-05 13:53:52 -0400

    added comments regarding variables

--- src/JavaVar.java
+++ src/JavaVar.java
@@ -2,17 +2,21 @@
 public class JavaVar 
 {
 // datatypes
+// class level vars or variables
+
     public static void main(String[] args) throws Exception 
     {
         // datatype varName [=value]; 
         int games = 3; 
         double allowance = 40.50;
         long battlepass; 
-
+        // We define battlepass
         battlepass = 15;
+        // We assign a value to battle pass
 
+        // Print out our vars or variable values along with a string that explains our values
         System.out.println("Number of games I own = " + games);
         System.out.println("My monthly allowance = " + allowance);
-        System.out.println("The cost of a single battlepass = " + battlepass);
+        System.out.println("The cost of a single battle pass = " + battlepass);
     }
 }
